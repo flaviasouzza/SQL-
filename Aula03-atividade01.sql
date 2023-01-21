73393af1302757d194564914ff72be0836beaa68
Flávia Souza <88092488+flaviasouzza@users.noreply.github.com>
2023-01-20 21:24:18 -0300

    Update Aula03-atividade01.sql

--- Aula03-atividade01.sql
+++ Aula03-atividade01.sql
@@ -10,14 +10,14 @@ INSERT INTO aluno(
 	hora_aula,
 	matriculado_em) 
 VALUES (
-	'Diogo',
-	'12345678901',
-	'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras finibus mollis justo in congue. Curabitur eget malesuada lorem, posuere varius ante. Mauris in est orci. Sed semper pretium velit vitae egestas. Donec finibus semper eleifend. Ut id velit ultricies, pellentesque purus nec, maximus risus. Nulla tristique tellus nec quam condimentum, sit amet convallis erat fermentum. Mauris in dictum dui. In et ante quam. Suspendisse tempor leo nec elementum mattis. Vestibulum rutrum quam risus, eu aliquam sem rhoncus eu.Vestibulum facilisis nunc at viverra luctus. Vestibulum quis tortor lacus. Sed at euismod mauris. Praesent sed neque risus. Donec sed cursus ligula. Nam cursus porttitor purus auctor consequat. In vitae turpis aliquet, euismod quam eu, finibus lacus. Curabitur volutpat tellus et pretium volutpat.',
-	35,
+	'Flvia',
+	'11122233344',
+	'Aluna Alura Teste',
+	28,
 	100.50,
-	1.81,
+	1.77,
 	TRUE,
-	'1984-08-27',
+	'1996-11-26',
 	'17:30:00',
 	'2020-02-08 12:32:45'
 );
